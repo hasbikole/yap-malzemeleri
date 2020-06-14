50aebf46cf988cb594e777e4774aa28b976dfaa4
hasbikole <66913242+hasbikole@users.noreply.github.com>
2020-06-14 23:46:15 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YapiMalzemeleriOdev.MvcApplication" Language="C#" %>
---
+++ ViewModels/KategoriFormViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YapiMalzemeleriOdev.Models.EntityFramework;
+
+namespace YapiMalzemeleriOdev.ViewModels
+{
+    public class KategoriFormViewModels
+    {
+        public IEnumerable<Kategori> Kategoriler { get; set; }
+        public Urunler urunler { get; set; }
+    }
+}
